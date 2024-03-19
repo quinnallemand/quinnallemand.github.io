ba669f2c650cd2d108e893460385a8678be2d188
quinnallemand <156845509+quinnallemand@users.noreply.github.com>
2024-03-19 14:05:37 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -17,112 +17,48 @@ let dropOuts = document.getElementById("dropdown-outs");
 
 //gathers information from the slider object on HTML and puts it in a variable called "slider"
 
+// this array will be effected and used to initialize our key
 let scale = [0,1,2,3,4,5,6,7,8,9,10,11,12]
-
+//this is linking our object to the button designated for major keys
 let majorButton = document.getElementById("majorScale")
-
+//this changes the array when the button is clicked
 majorButton.addEventListener("click", function() { 
-    scale[0] = 0;
-    scale[1] = 2;
-    scale[2] = 2;
-    scale[3] = 4;
-    scale[4] = 4;
-    scale[5] = 5;
-    scale[6] = 7;
-    scale[7] = 7;
-    scale[8] = 9;
-    scale[9] = 9;
-    scale[10] = 11;
-    scale[11] = 11;
-    scale[12] = 12;
+    scale = [0,2,4,5,7,9,11,12]
     console.log (scale);
   });
 
   let minorButton = document.getElementById("minorScale")
 
   minorButton.addEventListener("click", function() { 
-    scale[0] = 0;
-    scale[1] = 2;
-    scale[2] = 2;
-    scale[3] = 3;
-    scale[4] = 3;
-    scale[5] = 5;
-    scale[6] = 7;
-    scale[7] = 7;
-    scale[8] = 8;
-    scale[9] = 8;
-    scale[10] = 10;
-    scale[11] = 10;
-    scale[12] = 12;
+    scale = [0,2,3,5,7,8,10,12];
     console.log (scale);
   });
 
   let NoButton = document.getElementById("noScale")
 
   NoButton.addEventListener("click", function() { 
-    scale[0] = 0;
-    scale[1] = 1;
-    scale[2] = 2;
-    scale[3] = 3;
-    scale[4] = 4;
-    scale[5] = 5;
-    scale[6] = 6;
-    scale[7] = 7;
-    scale[8] = 8;
-    scale[9] = 9;
-    scale[10] = 10;
-    scale[11] = 11;
-    scale[12] = 12;
+   scale = [0,1,2,3,4,5,6,7,8,9,10,11,12];
     console.log (scale);
   });
 
   let majorPent = document.getElementById("majorPentScale")
 
   majorPent.addEventListener("click", function() { 
-    scale[0] = 0;
-    scale[1] = 2;
-    scale[2] = 2;
-    scale[3] = 4;
-    scale[4] = 4;
-    scale[5] = 7;
-    scale[6] = 7;
-    scale[7] = 7;
-    scale[8] = 9;
-    scale[9] = 9;
-    scale[10] = 9;
-    scale[11] = 12;
-    scale[12] = 12;
+    scale = [0,2,4,7,9,12];
     console.log (scale);
   });
 
   let minorPent = document.getElementById("minorPentScale")
 
   minorPent.addEventListener("click", function() { 
-    scale[0] = 0;
-    scale[1] = 0;
-    scale[2] = 3;
-    scale[3] = 3;
-    scale[4] = 3;
-    scale[5] = 5;
-    scale[6] = 5;
-    scale[7] = 7;
-    scale[8] = 7;
-    scale[9] = 7;
-    scale[10] = 10;
-    scale[11] = 10;
-    scale[12] = 12;
+    scale = [0,3,5,7,10,12];
     console.log (scale);
   });
+//this is a new array (it doesnt really need to be an array but thats ok) that I will use to store 
+let noteAdd = 0
 
-let noteAdd = [0]
 
-let numberOfBalls = document.getElementById("numberOfBalls")
 
-//function that creates a new number within the base set specified by the array "scale" and then adds the new number to "noteAdd"
-const randomizer = function () {
-  noteAdd[0] = scale[(Math.floor(Math.random()*scale.length))];
-  console.log(noteAdd);
-};
 
 
 // For each MIDI input device detected, add an option to the input devices dropdown.
@@ -151,12 +87,15 @@ const midiProcess = function (midiNoteInput) {
     return midiNoteOutput;
   };
 
-
+const randomizer = function () {
+  noteAdd = scale[(Math.floor(Math.random()*scale.length))];
+  myOutput.sendNoteOn(midiProcess(someMIDI))
+};
 // Add an event listener for the 'change' event on the input devices dropdown.
 // This allows the script to react when the user selects a different MIDI input device.
 dropIns.addEventListener("change", function () {
  // links the random number generator whever channel the user is using, and changes the number when a "noteon" is recognized.
-  const listeners = WebMidi.inputs[dropIns.value].addListener("noteon", randomizer,{channels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]});
+  // const listeners = WebMidi.inputs[dropIns.value].addListener("noteon", randomizer, {channels:[dropIns.value]});
   // Before changing the input device, remove any existing event listeners
   // to prevent them from being called after the device has been changed.
   if (myInput.hasListener("noteon")) {
@@ -168,15 +107,16 @@ dropIns.addEventListener("change", function () {
   myInput = WebMidi.inputs[dropIns.value];
  // After changing the input device, add new listeners for 'noteon' and 'noteoff' events.
   // These listeners will handle MIDI note on (key press) and note off (key release) messages.
-  myInput.addListener("noteon", function (someMIDI) {
-   
+  myInput.addListener("noteon", function (someMIDI) { 
     // When a note on event is received, send a note on message to the output device.
     // This can trigger a sound or action on the MIDI output device.
-    myOutput.sendNoteOn(midiProcess(someMIDI));
-     }); 
+    
+     myOutput.sendNoteOn(midiProcess(someMIDI));
+  }); 
   myInput.addListener("noteoff", function (someMIDI) {
     // Similarly, when a note off event is received, send a note off message to the output device.
     // This signals the end of a note being played.
+    console.log(someMIDI)
     myOutput.sendNoteOff(midiProcess(someMIDI));
       });
   });
@@ -189,7 +129,7 @@ dropOuts.addEventListener("change", function () {
   // Change the output device based on the user's selection in the dropdown.
   // The '.channels[1]' specifies that the script should use the first channel of the selected output device.
   // MIDI channels are often used to separate messages for different instruments or sounds.
-  myOutput = WebMidi.outputs[dropOuts.value];
+  myOutput = WebMidi.outputs[dropOuts.value.channels[1]];
 });
 
 
